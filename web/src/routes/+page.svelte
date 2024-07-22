c1d9d4140df4126f6913d81274acbf16348078f6
Tony-Villa <tonyvilla77@gmail.com>
2024-07-21 20:11:13 -0700

    chore: cleanup page boilerplate

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -4,9 +4,6 @@
   // console.log(data)
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 {#if !data.isLoggedIn}
   <form method="POST" action="?/login">
     <button formaction="?/login&provider=google">Google</button>
